8f6b213f0f1daf60aaf61743315bdeb70fcdfdb8
Michele Nuzzi <michele.nuzzi.2014@gmail.com>
2022-01-06 11:41:50 +0100

    getRewardAddresses CCvault function bad format fixed

--- src/Wallet/index.js
+++ src/Wallet/index.js
@@ -479,7 +479,7 @@ async function private_getCurrentUserDelegation( WalletProvider, blockfrost_proj
   // nami
   WalletProvider.getRewardAddress || 
   // CCVault
-  (async () => { return ( (await WalletProvider.getRewardAddresses())[0] ) });
+  WalletProvider.getRewardAddresses
 
   if( typeof getRewardAddress === "undefined" )
   throw WalletProcessError(
