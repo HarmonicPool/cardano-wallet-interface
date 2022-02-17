f3d71cc4d0b473cef00b36ca1b7a2c633d6adf91
Michele Nuzzi <michele.nuzzi.2014@gmail.com>
2022-02-17 21:40:02 +0100

    directly exports in error/index.js

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,25 +1,12 @@
 
-import StringFormatError from "./WalletInterfaceError/StringFormatError/StringFormatError";
+export * from "./WalletInterfaceError/StringFormatError/StringFormatError";
 
-import WalletProcessError from "./WalletInterfaceError/WalletProcessError/WalletProcessError";
-import WalletInterfaceError from "./WalletInterfaceError/WalletInterfaceError";
-import WalletError from "./WalletInterfaceError/WalletProcessError/WalletError/WalletError";
-import NamiError from "./WalletInterfaceError/WalletProcessError/WalletError/NamiError/NamiError";
-import CCVaultError from "./WalletInterfaceError/WalletProcessError/WalletError/CCVaultError/CCVaultError";
-import YoroiError from "./WalletInterfaceError/WalletProcessError/WalletError/YoroiError/YoroiError";
-import GeroError from "./WalletInterfaceError/WalletProcessError/WalletError/GeroError/GeroError";
-import FlintError from "./WalletInterfaceError/WalletProcessError/WalletError/FlintError/FlintError";
-import CardwalletError from "./WalletInterfaceError/WalletProcessError/WalletError/CardwalletError";
-
-export default {
-    WalletInterfaceError,
-    StringFormatError,
-    WalletProcessError,
-    WalletError,
-    CCVaultError,
-    NamiError,
-    YoroiError,
-    GeroError,
-    FlintError,
-    CardwalletError
-}
+export *  from "./WalletInterfaceError/WalletProcessError/WalletProcessError";
+export *  from "./WalletInterfaceError/WalletInterfaceError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/WalletError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/NamiError/NamiError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/CCVaultError/CCVaultError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/YoroiError/YoroiError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/GeroError/GeroError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/FlintError/FlintError";
+export *  from "./WalletInterfaceError/WalletProcessError/WalletError/CardwalletError";
